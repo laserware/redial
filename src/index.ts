db8241f68a7579efddc6a490b313ec292c1a9da8
Mike Rourke <mike.w.rourke@gmail.com>
2024-03-06 20:27:13 -0800

    chore: run Prettier and name GitHub Action job

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,5 @@ export {
   createForwardToMainMiddleware,
 } from "./middleware";
 export { replayActionInMain, replayActionInRenderer } from "./replay";
-export {
-  requestStateFromMain,
-  listenForStateRequests,
-} from "./syncState";
+export { requestStateFromMain, listenForStateRequests } from "./syncState";
 export { withRelay } from "./withRelay";
