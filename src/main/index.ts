55ec95759e0aaf317ee86af252dc3747c0d29258
Mike Rourke <mike.w.rourke@gmail.com>
2025-01-06 13:18:54 -0600

    docs: add module-level JSDoc comments to `main` and `renderer` modules

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,3 +1,34 @@
+/**
+ * Used to set up action forwarding in the Electron main process. Middleware
+ * must be added to both processes to ensure communication works.
+ *
+ * ## Usage
+ * ```ts
+ * import { createRedialMainMiddleware } from "@laserware/redial/main";
+ * import { configureStore, type Store } from "@reduxjs/toolkit";
+ * import { app } from "electron";
+ *
+ * import { rootReducer } from "../common/rootReducer";
+ *
+ * export function createStore(): Store {
+ *   const redialMiddleware = createRedialMainMiddleware();
+ *
+ *   const store = configureStore({
+ *     reducer: rootReducer,
+ *     middleware: (getDefaultMiddleware) =>
+ *       getDefaultMiddleware().concat(redialMiddleware),
+ *   });
+ *
+ *   app.on("before-quit", () => {
+ *     // Perform cleanup:
+ *     redialMiddleware.dispose();
+ *   });
+ * }
+ * ```
+ *
+ * @module main
+ */
+
 export {
   createRedialMainMiddleware,
   type RedialMainMiddleware,
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,3 +1,38 @@
+/**
+ * Used to set up action forwarding in the Electron renderer process. Middleware
+ * must be added to both processes to ensure communication works.
+ *
+ * ## Usage
+ * ```ts
+ * import { createRedialRendererMiddleware } from "@laserware/redial/renderer";
+ * import { configureStore, type Store } from "@reduxjs/toolkit";
+ *
+ * import { rootReducer } from "../common/rootReducer";
+ *
+ * // Assuming no context isolation:
+ * const ipcRenderer = window.require("electron").ipcRenderer;
+ *
+ * export function createStore(): Store {
+ *   const redialMiddleware = createRedialRendererMiddleware();
+ *
+ *   let preloadedState;
+ *   // If using Vite:
+ *   if (import.meta.env.MODE === "development") {
+ *     preloadedState = redialMiddleware.getMainStateSync();
+ *   }
+ *
+ *   const store = configureStore({
+ *     preloadedState,
+ *     reducer: rootReducer,
+ *     middleware: (getDefaultMiddleware) =>
+ *       getDefaultMiddleware().concat(redialMiddleware),
+ *   });
+ * }
+ * ```
+ *
+ * @module renderer
+ */
+
 export {
   createRedialRendererMiddleware,
   type IpcRendererMethods,
